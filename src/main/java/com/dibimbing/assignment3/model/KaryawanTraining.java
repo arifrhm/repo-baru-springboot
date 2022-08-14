207b5a3dbe17d6ac9a625cdde8f8d211f76f8a83
arifrhm <arif.rahman.rhm@gmail.com>
2022-08-15 00:39:31 +0700

    Initial commit

---
+++ src/main/java/com/dibimbing/assignment3/model/KaryawanTraining.java
@@ -0,0 +1,32 @@
+package com.dibimbing.assignment3.model;
+
+import lombok.Data;
+import org.hibernate.annotations.Where;
+import org.springframework.data.relational.core.mapping.Table;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "karyawan_training")
+@Where(clause = "deleted_date is null")
+public class KaryawanTraining extends AbstractDate implements Serializable {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "training_date")
+    private Date training_date;
+
+    @ManyToOne
+    @JoinColumn(name = "karyawan_id", referencedColumnName = "id")
+    private Karyawan karyawan;
+
+    @ManyToOne
+    @JoinColumn(name = "training_id", referencedColumnName = "id")
+    private Training training;
+}
---
+++ src/main/java/com/dibimbing/assignment3/service/KaryawanTrainingService.java
@@ -0,0 +1,12 @@
+package com.dibimbing.assignment3.service;
+
+import com.dibimbing.assignment3.model.KaryawanTraining;
+
+import java.util.Map;
+
+public interface KaryawanTrainingService {
+
+    public Map insert(KaryawanTraining request);
+
+    public Map delete(Long kry);
+}
